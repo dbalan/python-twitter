d36cff672a64b76b6735db02ec313bdd5b589012
Jeremy Low <jeremylow@gmail.com>
2016-01-16 09:49:52 -0500

    updates category object with standard methods and fixes iteritems() call for py3

--- twitter/category.py
+++ twitter/category.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python
 
+import json
+
 
 class Category(object):
-    """A class representing the suggested user category structure used by the twitter API.
+    """ A class representing the suggested user category structure used
+    by the twitter API.
 
-    The UserStatus structure exposes the following properties:
-
-      category.name
-      category.slug
-      category.size
+    The Category structure exposes the following properties:
+        category.name
+        category.slug
+        category.size
     """
 
     def __init__(self, **kwargs):
-        """An object to hold a Twitter suggested  user category .
+        """ An object to hold a Twitter suggested  user category.
         This class is normally instantiated by the twitter.Api class and
         returned in a sequence.
 
         Args:
-          name:
-            name of the category
-          slug:
-
-          size:
+            name:
+                Name of the category
+            slug:
+                Slugified name of Category (a-zA-Z0-9-_ only)
+            size:
+                Number of users in Category
         """
         param_defaults = {
             'name': None,
@@ -29,7 +32,7 @@ class Category(object):
             'size': None,
         }
 
-        for (param, default) in param_defaults.iteritems():
+        for (param, default) in param_defaults.items():
             setattr(self, param, kwargs.get(param, default))
 
     @property
@@ -44,14 +47,53 @@ class Category(object):
     def Size(self):
         return self.size or False
 
+    def __eq__(self, other):
+        return other and \
+            self.AsDict() == other.AsDict()
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __repr__(self):
+        """ A string representation of this twitter.Category instance.
+
+        Returns:
+            String representation containing name, slug, and size of
+            category.
+        """
+        return "Category(Name={name}, Slug={slug}, Size={size})".format(
+            name=self.name,
+            slug=self.slug,
+            size=self.size)
+
+    def __str__(self):
+        return self.AsJsonString()
+
+    def AsDict(self):
+        data = {}
+
+        if self.name:
+            data['name'] = self.name
+        if self.slug:
+            data['slug'] = self.slug
+        if self.size:
+            data['size'] = self.size
+
+        return data
+
+    def AsJsonString(self):
+        return json.dumps(self.AsDict(), sort_keys=True)
+
     @staticmethod
     def NewFromJsonDict(data):
         """Create a new instance based on a JSON dict.
 
         Args:
-          data: A JSON dict, as converted from the JSON in the twitter API
+            data: A JSON dict, as converted from the JSON in the twitter
+            API.
+
         Returns:
-          A twitter.Category instance
+            A twitter.Category instance
         """
 
         return Category(name=data.get('name', None),
