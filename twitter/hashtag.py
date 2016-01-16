e271894dd0b0c4230bcc135e69a8f9dc1316911c
Jeremy Low <jeremylow@gmail.com>
2016-01-16 13:15:30 -0500

    adds standard methods for twitter.Hashtag objects

--- twitter/hashtag.py
+++ twitter/hashtag.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import json
+
 
 class Hashtag(object):
     """ A class representing a twitter hashtag """
@@ -7,6 +9,30 @@ class Hashtag(object):
     def __init__(self,
                  text=None):
         self.text = text
+
+    def __eq__(self, other):
+        return other and \
+            self.text == other.text
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __str__(self):
+        return self.AsJsonString()
+
+    def __repr__(self):
+        return "Hashtag(Text={text})".format(text=self.text)
+
+    def AsDict(self):
+        data = {}
+
+        if self.text:
+            data['text'] = self.text
+
+        return data
+
+    def AsJsonString(self):
+        return json.dumps(self.AsDict(), sort_keys=True)
 
     @staticmethod
     def NewFromJsonDict(data):
@@ -20,3 +46,4 @@ class Hashtag(object):
           A twitter.Hashtag instance
         """
         return Hashtag(text=data.get('text', None))
+
